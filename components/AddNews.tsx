92cce7d9896ce28b0ba9be60a458cf0cd969924f
olga <khmarukolgaivanovna@gmail.com>
2024-05-22 12:17:39 +0300

    feat: create add news form

--- components/AddNews.tsx
+++ components/AddNews.tsx
@@ -1,6 +1,13 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Modal, TouchableOpacity, View, Text, TextInput } from "react-native";
-import { Formik } from "formik";
+import {
+  Modal,
+  TouchableOpacity,
+  View,
+  Text,
+  TextInput,
+  Button,
+} from "react-native";
+import { Formik, FormikHelpers, FormikValues } from "formik";
 
 interface Props {
   visible: boolean;
@@ -16,6 +23,44 @@ export const AddNews = ({ visible, onClose }: Props) => (
         <Ionicons name="close-circle" size={40} color="gray" />
       </TouchableOpacity>
     </View>
-    <View className="p-2 gap-2"></View>
+    <View className="p-2 gap-2">
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+          text: "",
+          img: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(props) => (
+          <View>
+            <TextInput
+              value={props.values.title}
+              placeholder="Title"
+              onChangeText={props.handleChange("title")}
+            />
+            <TextInput
+              value={props.values.description}
+              placeholder="Description"
+              onChangeText={props.handleChange("description")}
+            />
+            <TextInput
+              value={props.values.text}
+              placeholder="Text content"
+              onChangeText={props.handleChange("text")}
+            />
+            <TextInput
+              value={props.values.img}
+              placeholder="Image URL"
+              onChangeText={props.handleChange("img")}
+            />
+            <Button title="Добавить" onPress={() => props.handleSubmit()} />
+          </View>
+        )}
+      </Formik>
+    </View>
   </Modal>
 );
