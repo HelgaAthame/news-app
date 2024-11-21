ac412af9f5f68cad5daf38bbbb7ef4504ae75cbf
HelgaAthame <athamethedarkest@gmail.com>
2024-11-21 12:44:19 +0300

    feat: add vercel config

--- apollo/client.js
+++ apollo/client.js
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
   //uri: "http://127.0.0.1:3003/graphql", // Your NestJS GraphQL endpoint
-  uri: "https://newsapp-back-production.up.railway.app/graphql",
+  uri: "https://newsapp-back.vercel.app/graphql",
+  // uri: "https://newsapp-back-production.up.railway.app/graphql",
   credentials: "include", // Send cookies with every request
 });
 
