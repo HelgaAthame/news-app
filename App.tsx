cf0a3c951e06c1292260e780c2f1cb1c6a317d87
HelgaAthame <athamethedarkest@gmail.com>
2025-04-25 11:31:35 +0300

    feat: apdate app code

--- App.tsx
+++ App.tsx
@@ -1,3 +1,7 @@
+import "react-native-gesture-handler"; // если используешь react-navigation
+import { enableScreens } from "react-native-screens"; // вот это нужно
+enableScreens(); // активируем поддержку оптимизированных экранов
+
 import { Navigate } from "./navigate";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
