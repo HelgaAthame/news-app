befd7771e38de38031372c586a355bd7ec67c445
olga <khmarukolgaivanovna@gmail.com>
2026-07-23 00:41:21 -0400

    Fix typo in delete-confirmation dialog
    "Area you sure" -> "Are you sure"

--- components/DeleteNews.tsx
+++ components/DeleteNews.tsx
@@ -31,7 +31,7 @@ export const DeleteNews = ({ visible, onClose, id, name }: Props) => {
       <View className="flex-row flew-nowrap justify-between p-2 bg-red-50 grow">
         <View className="h-full flex items-center justify-center ">
           <Text className="text-2xl font-bold text-slate-700 text-center p-8">
-            Area you sure you want to delete {name}?
+            Are you sure you want to delete {name}?
           </Text>
         </View>
         <TouchableOpacity onPress={onClose} className="w-min ">
